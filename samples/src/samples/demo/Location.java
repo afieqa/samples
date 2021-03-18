d6803064c172d8a9763ae02af283949842b4d308
afieqa <afieqqqq@gmail.com>
2021-03-18 14:51:43 +0800

    Second Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,24 @@
+package samples.demo;
+
+public class Location {
+	
+	private String area;
+	private String state;
+	
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	public String getArea() {
+		return this.area;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getState() {
+		return this.state;
+	}
+
+}
